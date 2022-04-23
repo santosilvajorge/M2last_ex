45cdfecc2907532990dc06b3ce4c8336cc6103f7
santosilvajorge <santosilvajorge@gmail.com>
2022-04-23 22:29:48 +0100

    DOM 31

--- M2last_ex.js
+++ M2last_ex.js
@@ -109,3 +109,22 @@
        console.log(arrayB)
    }
    console.log(` Sum of Array A = ${sumA}, Sum of Array B = ${sumB}, since A higher than B, we will have as output the Array A`)
+
+
+   /* DOM EXERCISES
+
+        31) Get the element with an id of "container" from the page
+        32) Get every <td> element from the page
+        33) Use a loop for printing the text inside of every <td> element in the page
+        34) Write a function to change the heading of the page
+        35) Write a function to add an extra row to the table
+        36) Write a function to add a class of "test" to each row in the table
+        37) Write a function to add a red background to every link in the page
+        38) Console log "Page loaded" when the page is correctly loaded
+        39) Write a function to add new items to a unordered list
+        40) Write a function to empty a list */
+
+    //31
+    let container = document.getElementById('container')
+
+    //
